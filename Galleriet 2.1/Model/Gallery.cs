44e3519e4c15f648858e04bfb9cac6c915fa1639
MartinArvidsson <ma223ku@lnu.student.se>
2015-02-19 17:38:34 +0100

    Working ! Full speed spacemachine

---
+++ Galleriet 2.1/Model/Gallery.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace Galleriet_2._1.BOL
+{
+    public class Gallery
+    {
+        private static readonly Regex ApprovedExtensions;
+        private static string PhysicalUploadedImagesPath; //= "~/Martin 2.1 Galleri/Galleriet 2.1/Galleri";        
+        private static readonly Regex SanitizePath;
+        static Gallery()
+        {
+            ApprovedExtensions = new Regex(@"^.*\.(gif|jpg|png)$");
+            //AppDomain.CurrentDomain.GetData("APPBASE").ToString();
+            PhysicalUploadedImagesPath = Path.Combine(AppDomain.CurrentDomain.GetData("APPBASE").ToString(), "Galleri");
+
+            var invalidChars = new string(Path.GetInvalidFileNameChars());
+            SanitizePath = new Regex(string.Format("[{0}]", Regex.Escape(invalidChars)));    
+        }
+
+        IEnumerable<string> GetImageNames()
+        {
+            var directory = new DirectoryInfo(Path.Combine(PhysicalUploadedImagesPath, "Gallerithumb"));
+            FileInfo[] fileinfo = directory.GetFiles();
+            var imageList = fileinfo.Select(image => image.Name).Where(filename => ApprovedExtensions.IsMatch(filename)).ToList();
+
+            return imageList.AsReadOnly();
+        }
+
+        private static bool ImageExists(string name)
+        {
+            return File.Exists(Path.Combine(PhysicalUploadedImagesPath,name));
+        }
+
+        private static bool IsValidImage(Image Image)
+        {
+            return Image.RawFormat.Guid == System.Drawing.Imaging.ImageFormat.Gif.Guid || Image.RawFormat.Guid == System.Drawing.Imaging.ImageFormat.Jpeg.Guid || Image.RawFormat.Guid == System.Drawing.Imaging.ImageFormat.Png.Guid;
+        }
+
+        private static string SaveImage(Stream stream, string fileName)
+        {
+            var Image = System.Drawing.Image.FromStream(stream);
+
+            if(IsValidImage(Image))
+            {
+                var Thumbnail = Image.GetThumbnailImage(60, 45, null, System.IntPtr.Zero);
+
+                string newFileName = Path.GetFileNameWithoutExtension(fileName);
+                int i = 2;
+
+                while(ImageExists(newFileName + Path.GetExtension(fileName)))
+                {
+                    newFileName = Path.GetFileNameWithoutExtension(fileName);
+                    newFileName = newFileName + i;
+                    i++;
+                }
+                fileName = newFileName + Path.GetExtension(fileName);
+
+                Image.Save(PhysicalUploadedImagesPath + fileName);
+                //Thumbnail.Save (PhysicalUploadedImagesPath + "Gallerithumb/" + fileName);
+                Thumbnail.Save(Path.Combine(AppDomain.CurrentDomain.GetData("APPBASE").ToString(), "Galleri", "Gallerithumb"));
+
+                return fileName;
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+}
